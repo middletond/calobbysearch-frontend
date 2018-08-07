67a306921f2da6e97096a4358c6131fec83682fb
middletond <middletond@me.com>
2018-08-06 19:40:00 -0700

    SASS loader working (mostly)

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   entry: "./src/index.js",
@@ -17,11 +18,27 @@ module.exports = {
             presets: ["env", "react"]
           }
         }
+      },
+      {
+        test: /\.(s*)css$/,
+        use: [
+          // MiniCssExtractPlugin.loader,
+          "style-loader",
+          "css-loader",
+          "sass-loader"
+        ]
       }
     ]
   },
+  // plugins: [
+  //   new MiniCssExtractPlugin({
+  //     filename: "[name].css",
+  //     chunkFilename: "[id].css"
+  //   })
+  // ],
   mode: "development",
   devServer: {
-    contentBase: "./"
+    contentBase: "./",
+    port: 8001
   }
 };
