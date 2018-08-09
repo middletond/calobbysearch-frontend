f6f6e95972d3c41dabb40df7e4b6f41c0c7b36fc
middletond <middletond@me.com>
2018-08-09 08:37:24 -0700

    Result header data display, except for bill count

--- src/components/results/header.js
+++ src/components/results/header.js
@@ -5,13 +5,15 @@ const ResultsHeader = ({ results }) => {
     <div className="results-header">
       <h2 className="results-header-text">{
         ("params" in results) ?
-        `Records for <b>${results.params.company}</b> and <b>${results.params.bill}</b>` :
-        `Search for lobby activity by bill, company, or both.`
-    }</h2>
+        <span>Records for <b>{results.params.company}</b> and <b>{results.params.bill}</b></span> :
+        <span>Search for lobby activity by <b>bill</b>, <b>company</b>, or <b>both</b>.</span>
+     }</h2>
+    {("records" in results) ?
       <div className="results-header-meta">
-        <div className="meta-item">22 Filings</div>
-        <div className="meta-item">1 Bill</div>
-      </div>
+        <div className="meta-item">{results.records.length} Filings</div>
+        <div className="meta-item">{("totals" in results) ? results.totals.bills : 1} Bills</div>
+      </div> : ""
+    }
     </div>
   )
 }
