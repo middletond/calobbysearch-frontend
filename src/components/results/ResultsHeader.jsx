fbcbf8d39a4cdcbd82d11607e0df08f4f2cfdba3
middletond <middletond@me.com>
2018-08-13 10:17:35 -0700

    Make results header message smarter

--- src/components/results/ResultsHeader.jsx
+++ src/components/results/ResultsHeader.jsx
@@ -1,21 +1,38 @@
 import React from "react";
 
-const ResultsHeader = ({ results }) => {
-  return (
-    <div className="results-header">
-      <h2 className="results-header-text">{
-        ("params" in results) ?
-        <span>Records for <b>{results.params.company}</b> and <b>{results.params.bill}</b></span> :
-        <span>Search for lobby activity by <b>bill</b>, <b>company</b>, or <b>both</b>.</span>
-     }</h2>
-    {("records" in results) ?
-      <div className="results-header-meta">
-        <div className="meta-item">{results.records.length} Filings</div>
-        <div className="meta-item">{results.bills.length} Bills</div>
-      </div> : ""
+class ResultsHeader extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.headerMessage = this.headerMessage.bind(this);
+  }
+
+  headerMessage(results) {
+    if (!("params" in results)) 
+      return <span>Search for lobby activity by <b>bill</b>, <b>company</b>, or <b>both</b>.</span>;
+
+    const { bill, company } = results.params;
+
+    if (bill && company) {
+      return <span>Lobby activity for <b>{results.params.company}</b> and <b>{results.params.bill}</b></span>;
     }
-    </div>
-  )
+    else if (bill) {
+      return <span>Lobby activity for <b>{results.params.bill}</b></span>;
+    }
+    else if (company) {
+      return <span>Lobby activity for <b>{results.params.company}</b></span>;
+    }
+    return <span><b>Oops!</b> Neither bill nor company found in search.</span>
+  }
+
+  render() {
+    const { results } = this.props;
+    return (
+      <div className="results-header">
+        <h2 className="results-header-text">{this.headerMessage(results)}</h2>
+      </div>
+    )
+  }
 }
 
 export default ResultsHeader;
