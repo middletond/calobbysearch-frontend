4f85bacff160267ce1b75160600b99fc4d6ed363
middletond <middletond@me.com>
2018-08-13 11:00:30 -0700

    clean up filter field and add icon

--- src/components/controls/FilterField.jsx
+++ src/components/controls/FilterField.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 
+import Icon from "./Icon";
+
 const FilterField = ({ value, onChange }) => {
   return (
-    <input
-      className="filter-field"
-      placeholder="Type to filter results"
-      onChange={event => onChange(event.target.value)}
-      value={value} />
+    <div className="filter-field">
+      <Icon name="filter" />
+      <input
+        placeholder="type to filter results"
+        onChange={event => onChange(event.target.value)}
+        value={value} />
+    </div>
   )
 }
 
