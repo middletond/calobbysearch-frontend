b95d1cd278c6d540e3bd54316199fc3502a337f6
middletond <middletond@me.com>
2018-08-13 11:23:03 -0700

    Add dummy icon to export button.

--- src/components/controls/ExportButton.jsx
+++ src/components/controls/ExportButton.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 
+import Icon from "./Icon";
+
 const ExportButton = ({ onExport }) => {
   const label = "Export to CSV";
 
   return (
-    <button className="button export-button" onClick={onExport}>
-      {label}
-    </button>
+    <div className="export-button" onClick={onExport}>
+      <Icon name="download" />
+      <button className="button">
+        {label}
+      </button>
+    </div>
   )
 }
 
