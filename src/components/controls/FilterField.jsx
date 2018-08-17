bb3199f7bb2d81c6259fa74269ab2fb45c8932aa
middletond <middletond@me.com>
2018-08-17 15:28:14 -0700

    filter SVG icon + filter UI tweaks

--- src/components/controls/FilterField.jsx
+++ src/components/controls/FilterField.jsx
@@ -5,7 +5,7 @@ import Icon from "./Icon";
 const FilterField = ({ value, onChange }) => {
   return (
     <div className="filter-field">
-      <Icon name="filter" />
+      <Icon name="filter" active={(value) ? true : false} />
       <input
         placeholder="type to filter results"
         onChange={event => onChange(event.target.value)}
--- src/components/controls/Icon.jsx
+++ src/components/controls/Icon.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 
-const Icon = ({ name }) => {
+const Icon = ({ name, active }) => {
   return (
-    <div className="icon">
+    <div className={(active) ? "icon active" : "icon"}>
       {(name in SVG_ICON_PATHS)
       ? <svg viewBox="0 0 30 30">
           <path d={SVG_ICON_PATHS[name]} />
@@ -17,5 +17,10 @@ export default Icon;
 const SVG_ICON_PATHS = {
   "export": `M 2,19 L 2,24 L 28,24 L 28,19
              M 15,2 L 15,15
-             M 10,11.5 L 15,17 L 20,11.5`
+             M 10,11.5 L 15,17 L 20,11.5`,
+
+  // "filter": `M2,2 L2,4 L11,13 L11,24
+  //            M28,2 L28,4 L19,13 L19,24`
+  "filter": `M2,4 L11,13 L11,24
+             M28,4 L19,13 L19,28`
 }
