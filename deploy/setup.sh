a9a0b8bf81f8037574ba91b417ba4a5c434b6e6f
middletond <middletond@me.com>
2018-08-27 14:26:21 -0700

    Add bashrc tweak

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -6,9 +6,9 @@ SITE_ROOT="$( cd "$( dirname "${BASH_SOURCE[1]}" )" && pwd )"
 
 NGINX_CONF_SOURCE=$SITE_ROOT"/deploy/nginx.conf"
 NGINX_CONF_DEST="/etc/nginx/sites-enabled/default"
-
 NODE_PPA_SCRIPT="nodesource_setup.sh"
 
+echo "Setting up app with root $SITEROOT"
 touch setup.log
 
 echo "Updating package manager."
@@ -33,3 +33,8 @@ sudo apt-get install -y nginx >> setup.log
 echo "Linking $NGINX_CONF_SOURCE to $NGINX_CONF_DEST"
 sudo rm $NGINX_CONF_DEST
 sudo ln -s $NGINX_CONF_SOURCE $NGINX_CONF_DEST >> setup.log
+
+echo "Adding to .bashrc for easier ssh."
+echo "cd $SITE_ROOT" >> ~/.bashrc
+
+echo "Server is set up."
