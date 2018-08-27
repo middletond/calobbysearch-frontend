96a9403f138c88b71b59b0140a6730fda7862aa0
middletond <middletond@me.com>
2018-08-27 16:44:52 -0700

    better name for cert script

--- deploy/cert.sh
+++ deploy/cert.sh
@@ -5,12 +5,11 @@
 sudo touch certbot.log
 
 echo "Update apt repo..."
-sudo apt-get update >> certbot.log
-sudo apt-get install software-properties-common >> certbot.log
-sudo add-apt-repository ppa:certbot/certbot >> certbot.log
+sudo apt-get install -y software-properties-common >> certbot.log
+sudo add-apt-repository -y ppa:certbot/certbot >> certbot.log
 sudo apt-get update >> certbot.log
 echo "Installing certbot package."
-sudo apt-get install python-certbot-nginx  >> certbot.log
+sudo apt-get install -y python-certbot-nginx  >> certbot.log
 
 echo "Install SSL cert and configuring nginx."
 sudo service nginx stop # nginx must be turned off to install the cert
@@ -18,7 +17,7 @@ sudo certbot --nginx
 sudo service nginx start
 
 # Or only install the cert and configure by hand:
-# sudo certbot --nginx ertonly
+# sudo certbot --nginx certonly
 
 echo "Add the following to crontab to check for renewal (and renew when ready) daily:"
 echo "0 0 * * * /usr/bin/certbot renew"
