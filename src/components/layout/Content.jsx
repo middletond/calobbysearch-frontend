4aa703984c42196d7b00afd174d1cc0ecd3f167b
middletond <middletond@me.com>
2018-09-04 08:37:46 -0700

    No mobile users for now

--- src/components/layout/Content.jsx
+++ src/components/layout/Content.jsx
@@ -1,9 +1,19 @@
 import React from "react";
+import { isMobile } from 'react-device-detect';
+
 
 import SearchForm from "../../containers/SearchForm";
 import Results from "../../containers/Results";
 
 const Content = () => {
+  if (isMobile && window.outerWidth) {
+    return (
+      <main className="content mobile-message">
+        <h2>So sorry.</h2>
+        <h3>This tool is currently available for <b>desktops and laptops only</b>.</h3>
+      </main>
+    )
+  }
   return (
     <main className="content">
       <SearchForm />
