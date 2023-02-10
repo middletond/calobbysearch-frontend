c70ef18de052cfeeef4f82633e11a946cd333493
middletond <middletond@me.com>
2023-02-10 12:32:18 -0800

    Chore: add 2023-24 session

--- src/constants.js
+++ src/constants.js
@@ -5,6 +5,7 @@ export const USE_DATES_NOT_SESSION = "USE_DATES";
 export const DATE_PICKER_FIELDS = ["startDate", "endDate"];
 
 export const SESSIONS = [ // XXX Dummy data: move this to API call
+  { value: "20232024", verbose: "2023 - 2024" },
   { value: "20212022", verbose: "2021 - 2022" },
   { value: "20192020", verbose: "2019 - 2020" },
   { value: "20172018", verbose: "2017 - 2018" },
